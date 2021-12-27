ab9247edcae6fdbf6ab651f09e496163ad59020f
roluochKe <rasholuoch@gmail.com>
2021-12-27 13:18:38 +0300

    product details

--- components/Layout.js
+++ components/Layout.js
@@ -1,20 +1,35 @@
 import React from 'react'
 import Head from 'next/head'
-import { AppBar, Container, Toolbar, Typography } from '@material-ui/core'
+import NextLink from 'next/link'
+import { AppBar, Container, Link, Toolbar, Typography } from '@material-ui/core'
 
 import useStyles from '../utils/styles'
 
-export const Layout = ({ children }) => {
+export const Layout = ({ title, description, children }) => {
   const classes = useStyles()
 
   return (
     <div>
       <Head>
-        <title>Next Amazon</title>
+        <title>{title ? `${title} - Next Amazon` : 'Next Amazon'}</title>
+        {description && <meta name='description' content={description}></meta>}
       </Head>
       <AppBar position='static' className={classes.navbar}>
         <Toolbar>
-          <Typography>Next Amazon</Typography>
+          <NextLink href='/' passHref>
+            <Link>
+              <Typography className={classes.brand}>Next Amazon</Typography>
+            </Link>
+          </NextLink>
+          <div className={classes.grow}></div>
+          <div>
+            <NextLink href='/cart' passHref>
+              <Link>Cart</Link>
+            </NextLink>
+            <NextLink href='/login' passHref>
+              <Link>Login</Link>
+            </NextLink>
+          </div>
         </Toolbar>
       </AppBar>
       <Container className={classes.main}>{children}</Container>
