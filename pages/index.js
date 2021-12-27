63d0399805a46511aaedb035b1304200b58d051c
roluochKe <rasholuoch@gmail.com>
2021-12-27 16:50:09 +0300

    checkout page

--- pages/index.js
+++ pages/index.js
@@ -1,3 +1,4 @@
+import { useContext } from 'react'
 import {
   Button,
   Card,
@@ -11,10 +12,29 @@ import {
 import NextLink from 'next/link'
 import { Layout } from '../components/Layout'
 import db from '../utils/db'
+import axios from 'axios'
+import { Store } from '../utils/Store'
+import { useRouter } from 'next/router'
 import Product from '../models/Product'
 
 export default function Home(props) {
+  const router = useRouter()
+  const { state, dispatch } = useContext(Store)
   const { products } = props
+
+  const addToCartHandler = async (product) => {
+    const existItem = state.cart.cartItems.find((x) => x._id === product._id)
+    const quantity = existItem ? existItem.quantity + 1 : 1
+    const { data } = await axios.get(`/api/products/${product._id}`)
+
+    if (data.countInStock < quantity) {
+      window.alert('Sorry. Product is out of stock')
+      return
+    }
+
+    dispatch({ type: 'CART_ADD_ITEM', payload: { ...product, quantity } })
+    router.push('/cart')
+  }
 
   return (
     <Layout>
@@ -37,7 +57,11 @@ export default function Home(props) {
               </NextLink>
               <CardActions>
                 <Typography>${product.price}</Typography>
-                <Button size='small' color='primary'>
+                <Button
+                  size='small'
+                  color='primary'
+                  onClick={() => addToCartHandler(product)}
+                >
                   Add to Cart
                 </Button>
               </CardActions>
